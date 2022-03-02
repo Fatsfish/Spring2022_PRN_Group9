04915a40117656f7a051116bd832191487193d9d
tonynguyen2512 <nguyenhathien2512@gmail.com>
2022-03-02 11:17:09 +0700

    Create Sollution

---
+++ EMS.Razor/EMS.Razor/Models​/AllowedEventGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EMS.Razor.Models​
+{
+    public partial class AllowedEventGroup
+    {
+        public int? EventId { get; set; }
+        public int? GroupId { get; set; }
+
+        public virtual Event Event { get; set; }
+        public virtual Group Group { get; set; }
+    }
+}
