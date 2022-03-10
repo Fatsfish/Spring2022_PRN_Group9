bfffe876ff9d4d68121358bb9aec5ef128d8821f
tonynguyen2512 <72120353+tonynguyen2512@users.noreply.github.com>
2022-03-10 19:37:07 +0700

    Bootstrap (#4)
    * Create Sollution
    
    * Update Create.cshtml
    
    * Create CRUD
    
    * Bootstrap
    
    * Pagination

--- EMS/EMS/Pages/InvitationResponseType/Index.cshtml.cs
+++ EMS/EMS/Pages/InvitationResponseType/Index.cshtml.cs
@@ -6,23 +6,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EMS.Models;
+using Microsoft.Extensions.Configuration;
+using EMS.Services;
 
 namespace EMS.Pages.InvitationResponseType
 {
     public class IndexModel : PageModel
     {
         private readonly EMS.Models.EventMSContext _context;
-
-        public IndexModel(EMS.Models.EventMSContext context)
+        private readonly IConfiguration Configuration;
+        public IndexModel(EventMSContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
-        public IList<Models.InvitationResponseType> InvitationResponseType { get;set; }
+        public PaginatedList<Models.InvitationResponseType> InvitationResponseType { get;set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            InvitationResponseType = await _context.InvitationResponseTypes.ToListAsync();
+            var invitationResponseType = from m in _context.InvitationResponseTypes
+                .Include(p => p.EventInvitations)
+                .Include(p => p.Name)
+                           select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                invitationResponseType = invitationResponseType.Where(o => o.Name.Contains(SearchString));
+            }
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            InvitationResponseType = await PaginatedList<Models.InvitationResponseType>.CreateAsync(
+                invitationResponseType.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
--- EMS/EMS/Pages/UserRole/Index.cshtml.cs
+++ EMS/EMS/Pages/UserRole/Index.cshtml.cs
@@ -6,25 +6,48 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EMS.Models;
+using Microsoft.Extensions.Configuration;
+using EMS.Services;
 
 namespace EMS.Pages.UserRole
 {
     public class IndexModel : PageModel
     {
         private readonly EMS.Models.EventMSContext _context;
-
-        public IndexModel(EMS.Models.EventMSContext context)
+        private readonly IConfiguration Configuration;
+        public IndexModel(EventMSContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
-        public IList<Models.UserRole> UserRole { get;set; }
+        public PaginatedList<Models.UserRole> UserRole { get;set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            UserRole = await _context.UserRoles
-                .Include(u => u.Role)
-                .Include(u => u.User).ToListAsync();
+            var userRole = from m in _context.UserRoles
+                .Include(p => p.User)
+                .Include(p => p.Role)
+                        select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                userRole = userRole.Where(o => o.Role.Description.Contains(SearchString) || o.User.Bio.Contains(SearchString));
+            }
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            UserRole = await PaginatedList<Models.UserRole>.CreateAsync(
+                userRole.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
