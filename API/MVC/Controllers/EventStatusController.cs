09cc79237cf8d7a927eb7beebba6eaa96a44e047
Nguyễn Dũng <dpron12345@gmail.com>
2022-03-17 14:10:05 +0700

    generate model, controller, view

---
+++ API/MVC/Controllers/EventStatusController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class EventStatusController : Controller
+    {
+        private readonly EventMSContext _context;
+
+        public EventStatusController(EventMSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EventStatus
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.EventStatuses.ToListAsync());
+        }
+
+        // GET: EventStatus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventStatus = await _context.EventStatuses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (eventStatus == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventStatus);
+        }
+
+        // GET: EventStatus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventStatus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] EventStatus eventStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(eventStatus);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(eventStatus);
+        }
+
+        // GET: EventStatus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventStatus = await _context.EventStatuses.FindAsync(id);
+            if (eventStatus == null)
+            {
+                return NotFound();
+            }
+            return View(eventStatus);
+        }
+
+        // POST: EventStatus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] EventStatus eventStatus)
+        {
+            if (id != eventStatus.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(eventStatus);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventStatusExists(eventStatus.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(eventStatus);
+        }
+
+        // GET: EventStatus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventStatus = await _context.EventStatuses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (eventStatus == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventStatus);
+        }
+
+        // POST: EventStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventStatus = await _context.EventStatuses.FindAsync(id);
+            _context.EventStatuses.Remove(eventStatus);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventStatusExists(int id)
+        {
+            return _context.EventStatuses.Any(e => e.Id == id);
+        }
+    }
+}
